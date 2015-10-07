fe3d47cbba1fc2c4e354b6d4665fb599e4e57fbe
Hau Nguyen <hau100993@gmail.com>
2015-10-07 23:18:05 +0700

    Change data to test size of type

--- C_examples/type_in_C.c
+++ C_examples/type_in_C.c
@@ -6,20 +6,29 @@ FILE *file ;
 const char filepath[]= "/home/accedian/tmp/resuts.txt" ;
 
 void main ()  {
-unsigned int uint32  ;
-unsigned long long int uint64 ;
-uint64 = UINT_MAX +2;
-uint32 = (unsigned int)uint64 ;
- //unsigned int a = uint32 ;
+//unsigned int uint32  ;
+//unsigned long long int uint64 ;
+//uint64 = UINT_MAX +2;
+//uint32 = (unsigned int)uint64 ;
+
+unsigned long long int byte = 570055492992;
+
+unsigned long long int packet =4454678636
+ ;
+ 
+printf(" byte %llu \n" , byte ) ;
+printf(" pkg  %llu \n" , packet ) ;
+printf(" byte/pkg %llu \n", (byte/packet));
 
 
-file = fopen(filepath, "wb") ;
+//file = fopen(filepath, "wb") ;
 
-fprintf (file, "Value of uint64  %llu  \n ", uint64 );
-fprintf (file, "Value of uint32  %u    \n ", uint32 ) ;
-fopen(filepath, "r") ;
-printf( " Contents in file results %s : \n ", filepath) ;
+//fprintf (file, "Value of uint64  %llu  \n ", uint64 );
+//fprintf (file, "Value of uint32  %u    \n ", uint32 ) ;
 
-fclose (file) ;
+//fopen(filepath, "r") ;
+//printf( " Contents in file results %s : \n ", filepath) ;
+
+//fclose (file) ;
 
 }
